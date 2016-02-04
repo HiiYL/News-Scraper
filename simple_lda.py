466de348c99e496735273b5230d95c1dbe7b3aa1
HiiYL <yonglian146@gmail.com>
2016-02-04 12:04:22 +0800

    Added argparse and tidied up the lda script

--- simple_lda.py
+++ simple_lda.py
@@ -12,13 +12,36 @@ from nltk.stem import *
 import unicodecsv
 import re
 
+import argparse
+
+
+list_of_stemmer_choices = ["none", "porter", "porter2"]
+parser = argparse.ArgumentParser(description='run LDA on an input csv file.')
+parser.add_argument('-i','--input',dest="filename", help='input CSV file', required=True)
+parser.add_argument('-s','--stemmer', help='pick stemmer', default="porter2", choices=list_of_stemmer_choices)
+parser.add_argument('-ni','--num_iter', help='number of iterations', default="1000")
+
+args = parser.parse_args()
+
 _digits = re.compile('\d')
 def contains_digits(d):
     return bool(_digits.search(d))
 
-tokenizer = RegexpTokenizer(r'\w+')
-en_stop = get_stop_words('en')
-f = open("soya_dataset_year.csv")
+def process_tokens(tokens,stemmer):
+  tokens = [i for i in tokens if not i in en_stop and not contains_digits(i)]
+  if stemmer == 'porter':
+    tokens = [stem(i) for i in tokens]
+  elif stemmer == 'porter2':
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(i) for i in tokens]
+
+  return tokens
+
+
+
+
+
+f = open(args.filename)
 reader = unicodecsv.reader(f, encoding='utf-8')
 # csv_length = sum(1 for row in reader)
 # f.seek(0) #reset reader position
@@ -31,19 +54,19 @@ contents = [ row[contents_idx] for row in reader if row[contents_idx] ]
 f.seek(0)
 reader.next()
 titles = [ row[title_idx] for row in reader if row[contents_idx] ]
+
+
+
 texts = list()
-stemmer = PorterStemmer()
+tokenizer = RegexpTokenizer(r'\w+')
+en_stop = get_stop_words('en')
 for idx,i in enumerate(contents):
-  print "Processing Article #{} ".format(idx)
+  if not idx % 10:
+    print "INFO: Tokenizing articles <{}> ".format(idx)
   raw = i.lower()
   tokens = tokenizer.tokenize(raw)
-  stopped_tokens = [i for i in tokens if not i in en_stop and not contains_digits(i)]
-  #stem tokens
-  stemmed_tokens = [stem(i) for i in stopped_tokens]
-  texts.append(stemmed_tokens)
+  texts.append(process_tokens(tokens, args.stemmer))
   # print idx
-
-
   # add tokens to list
 
 print "[DEBUG] Length of Texts : {}".format(len(texts))
@@ -54,8 +77,11 @@ X = np.zeros((len(contents), len(dictionary)), dtype=np.int)
 for idx,i in enumerate(corpus):
   for j in i:
     X[idx][j[0]] = j[1]
-model = lda.LDA(n_topics=20, n_iter=1000, random_state=1)
+
+model = lda.LDA(n_topics=20, n_iter=int(args.num_iter), random_state=1)
 model.fit(X)
+
+
 n_top_words = 8
 topic_word = model.topic_word_  # model.components_ also works
 for i, topic_dist in enumerate(topic_word):
