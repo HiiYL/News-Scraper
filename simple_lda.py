165b7777b1e485296b0782a4b93d47da556682d6
HiiYL <yonglian146@gmail.com>
2016-02-04 16:32:13 +0800

    Added sample

--- simple_lda.py
+++ simple_lda.py
@@ -38,45 +38,47 @@ def process_tokens(tokens,stemmer):
   return tokens
 
 
+if args.filename == "sample":
+  X = lda.datasets.load_reuters()
+  dictionary = lda.datasets.load_reuters_vocab()
+  titles = lda.datasets.load_reuters_titles()
+else:
+  f = open(args.filename)
+  reader = unicodecsv.reader(f, encoding='utf-8')
+  # csv_length = sum(1 for row in reader)
+  # f.seek(0) #reset reader position
+  identifiers = reader.next()
+  contents_idx = identifiers.index("contents")
+  title_idx = identifiers.index("title")
+
+  contents = [ row[contents_idx] for row in reader if row[contents_idx] ]
+
+  f.seek(0)
+  reader.next()
+  titles = [ row[title_idx] for row in reader if row[contents_idx] ]
 
 
 
-f = open(args.filename)
-reader = unicodecsv.reader(f, encoding='utf-8')
-# csv_length = sum(1 for row in reader)
-# f.seek(0) #reset reader position
-identifiers = reader.next()
-contents_idx = identifiers.index("contents")
-title_idx = identifiers.index("title")
+  texts = list()
+  tokenizer = RegexpTokenizer(r'\w+')
+  en_stop = get_stop_words('en')
+  for idx,i in enumerate(contents):
+    if not idx % 10:
+      print "INFO: Tokenizing articles <{}> ".format(idx)
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+    texts.append(process_tokens(tokens, args.stemmer))
+    # print idx
+    # add tokens to list
 
-contents = [ row[contents_idx] for row in reader if row[contents_idx] ]
+  print "[DEBUG] Length of Texts : {}".format(len(texts))
+  dictionary = corpora.Dictionary(texts)
+  corpus = [dictionary.doc2bow(text) for text in texts]
 
-f.seek(0)
-reader.next()
-titles = [ row[title_idx] for row in reader if row[contents_idx] ]
-
-
-
-texts = list()
-tokenizer = RegexpTokenizer(r'\w+')
-en_stop = get_stop_words('en')
-for idx,i in enumerate(contents):
-  if not idx % 10:
-    print "INFO: Tokenizing articles <{}> ".format(idx)
-  raw = i.lower()
-  tokens = tokenizer.tokenize(raw)
-  texts.append(process_tokens(tokens, args.stemmer))
-  # print idx
-  # add tokens to list
-
-print "[DEBUG] Length of Texts : {}".format(len(texts))
-dictionary = corpora.Dictionary(texts)
-corpus = [dictionary.doc2bow(text) for text in texts]
-
-X = np.zeros((len(contents), len(dictionary)), dtype=np.int)
-for idx,i in enumerate(corpus):
-  for j in i:
-    X[idx][j[0]] = j[1]
+  X = np.zeros((len(contents), len(dictionary)), dtype=np.int)
+  for idx,i in enumerate(corpus):
+    for j in i:
+      X[idx][j[0]] = j[1]
 
 model = lda.LDA(n_topics=20, n_iter=int(args.num_iter), random_state=1)
 model.fit(X)
@@ -85,7 +87,10 @@ model.fit(X)
 n_top_words = int(args.topwords_count)
 topic_word = model.topic_word_  # model.components_ also works
 for i, topic_dist in enumerate(topic_word):
-  topic_words = [ dictionary[x] for x in np.array(dictionary)[np.argsort(topic_dist)][:-(n_top_words+1):-1] ]
+  if args.filename == "sample":
+    topic_words = np.array(dictionary)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
+  else:
+    topic_words = [ dictionary[x] for x in np.array(dictionary)[np.argsort(topic_dist)][:-(n_top_words+1):-1] ]
   print u'Topic {}: {}'.format(i, ' '.join(topic_words))
 doc_topic = model.doc_topic_
 for i in range(10):
