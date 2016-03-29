089cc3d57b0ae5c77826b22d020d774027763e63
HiiYL <yonglian146@gmail.com>
2016-03-29 10:38:45 +0800

    tidied up root directory

--- simple_lda.py
+++ simple_lda.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import lda
-import lda.datasets
 import csv
 from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
@@ -14,12 +13,16 @@ import re
 import os
 # import pyLDAvis.gensim
 import gensim
-
 import argparse
+
+# Playing around with just dictionary words
+# Using PyEnchant spell checker purpose
+import enchant
 
 
 list_of_stemmer_choices = ["none", "porter", "porter2", "lemma"]
 list_of_model_choices = ["lda", "dtm"]
+list_of_dictionary_choices = ["none", "technology", "automotive"]
 parser = argparse.ArgumentParser(description='run LDA on an input csv file.')
 parser.add_argument('-i','--input',dest="filename", help='input CSV file', required=True)
 parser.add_argument('-s','--stemmer', help='pick stemmer', default="lemma", choices=list_of_stemmer_choices)
@@ -27,24 +30,32 @@ parser.add_argument('-ni','--num_iter', help='number of iterations', default="10
 parser.add_argument('-ntw','--num_top_words', help='number of top_words', default="8")
 parser.add_argument('-nt','--num_topics', help='number of topics', default="10")
 parser.add_argument('-m', '--model', help='model used', default='dtm', choices=list_of_model_choices)
+parser.add_argument('-d', '--dictionary', help='dictionary used', default='none', choices=list_of_dictionary_choices)
+
 args = parser.parse_args()
 
 dir = os.getcwd()
+model_dir = os.path.join(dir, 'models/')
+dataset_dir = os.path.join(dir, 'datasets/')
+dictionary_dir = os.path.join(dir, 'dictionaries/')
+
 
 _digits = re.compile('\d')
 def contains_digits(d):
     return bool(_digits.search(d))
 
-# Playing around with just dictionary words
-# Using PyEnchant spell checker purpose
-import enchant
+
+def get_dict_dir():
+  return os.path.join(dictionary_dir, args.dictionary + "-english")
+
 d = enchant.Dict("en_US")
 # Or using the /usr/share/dict/british-english word list
-with open("technology-english") as word_file:
-  english_words = set(word.strip().lower() for word in word_file)
-  # print(english_words)
-  def is_english_word(word):
-    return word.lower() in english_words
+if args.dictionary != "none":
+  with open(get_dict_dir()) as word_file:
+    english_words = set(word.strip().lower() for word in word_file)
+    # print(english_words)
+    def is_english_word(word):
+      return word.lower() in english_words
 
 def process_tokens(tokens,stemmer):
   tokens = [i for i in tokens if not i in en_stop and not contains_digits(i) and is_english_word(i)]
@@ -59,6 +70,11 @@ def process_tokens(tokens,stemmer):
   return tokens
 
 
+def get_model_with_arguments_filename():
+  return args.filename.split('.')[0] + "_" + args.stemmer + "_" + args.num_iter + "_" + args.num_top_words + "_" + args.num_topics  + "_" + args.model
+
+
+
 if args.filename == "sample":
   X = lda.datasets.load_reuters()
   dictionary = lda.datasets.load_reuters_vocab()
@@ -69,12 +85,12 @@ else:
   #   for j in i:
   #     X[idx][j[0]] = j[1]
 
-  model_filename = os.path.join(dir, 'models/' + args.filename.split('.')[0] + "_" + args.model + '.model')
+  model_filename = os.path.join(model_dir, get_model_with_arguments_filename())
   print model_filename
   try:
     ldamodel = models.LdaModel.load(model_filename)
   except IOError:
-    dataset_filepath = os.path.join(dir, 'datasets/' + args.filename)
+    dataset_filepath = os.path.join(dataset_dir, args.filename)
     f = open(dataset_filepath)
     reader = unicodecsv.reader(f, encoding='utf-8')
     # csv_length = sum(1 for row in reader)
@@ -88,9 +104,6 @@ else:
     f.seek(0)
     reader.next()
     titles = [ row[title_idx] for row in reader if row[contents_idx] ]
-
-
-
     texts = list()
     tokenizer = RegexpTokenizer(r'\w+')
     en_stop = get_stop_words('en')
