4ea8989368b94c25ebfd542b811486a06a6bee51
HiiYL <yonglian146@gmail.com>
2016-03-31 22:13:15 +0800

    Fixed topic number and argument not affecting values, improved saving of models to record argument values

--- simple_lda.py
+++ simple_lda.py
@@ -15,22 +15,21 @@ import os
 import gensim
 import argparse
 
-# Playing around with just dictionary words
 # Using PyEnchant spell checker purpose
 import enchant
 
 
 list_of_stemmer_choices = ["none", "porter", "porter2", "lemma"]
 list_of_model_choices = ["lda", "dtm"]
-list_of_dictionary_choices = ["none", "technology", "automotive"]
+list_of_dictionary_choices = ["none", "technology", "automotive", "english"]
 parser = argparse.ArgumentParser(description='run LDA on an input csv file.')
 parser.add_argument('-i','--input',dest="filename", help='input CSV file', required=True)
 parser.add_argument('-s','--stemmer', help='pick stemmer', default="lemma", choices=list_of_stemmer_choices)
-parser.add_argument('-ni','--num_iter', help='number of iterations', default="1000")
+parser.add_argument('-ni','--num_iter', help='number of iterations', default="20")
 parser.add_argument('-ntw','--num_top_words', help='number of top_words', default="8")
 parser.add_argument('-nt','--num_topics', help='number of topics', default="10")
-parser.add_argument('-m', '--model', help='model used', default='dtm', choices=list_of_model_choices)
-parser.add_argument('-d', '--dictionary', help='dictionary used', default='none', choices=list_of_dictionary_choices)
+parser.add_argument('-m', '--model', help='model used', default='lda', choices=list_of_model_choices)
+parser.add_argument('-d', '--dictionary', help='dictionary used', default='english', choices=list_of_dictionary_choices)
 
 args = parser.parse_args()
 
@@ -51,6 +50,7 @@ def get_dict_dir(s):
 
 def get_exec_dir(s):
   return os.path.join(executable_dir, s)
+
 d = enchant.Dict("en_US")
 # Or using the /usr/share/dict/british-english word list
 if args.dictionary != "none":
@@ -124,25 +124,25 @@ else:
     dictionary = corpora.Dictionary(texts)
     corpus = [dictionary.doc2bow(text) for text in texts]
     # my_timeslices = [500,500,500,500,500,346]
-    my_timeslices = [100,100,20]
+    my_timeslices = [500,500,500,500,500, 346]
 
     if(args.model == "lda"):
-     ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary, passes=20)
+     ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=int(args.num_topics), id2word = dictionary, passes=int(args.num_iter))
     elif(args.model == "dtm"):
-      ldamodel = gensim.models.wrappers.DtmModel(get_exec_dir('dtm-darwin64'), corpus, my_timeslices, num_topics=20, id2word=dictionary,initialize_lda=True)
+      ldamodel = gensim.models.wrappers.DtmModel(get_exec_dir('dtm-darwin64'), corpus, my_timeslices, num_topics=int(args.num_topics), id2word=dictionary,initialize_lda=True)
     else:
       raise ValueError('Unknown Model Type')
 
     ldamodel.save(model_filename)
 
 if(args.model == "lda"):
-  for topic in ldamodel.show_topics(num_topics=10, num_words=10, log=False, formatted=False):
+  for topic in ldamodel.show_topics(num_topics=args.num_topics, num_words=int(args.num_top_words), log=False, formatted=False):
     print "Topic #" + str(topic[0]) + " :",
     for word in topic[1]:
       print word[0],
     print
 else:
-  for idx, topic in enumerate(ldamodel.show_topics(topics=10, topn=10, formatted=False)):
+  for idx, topic in enumerate(ldamodel.show_topics(topics=args.num_topics, topn=int(args.num_top_words),times=1, formatted=False)):
     print "Topic #" + str(idx) + " :",
     for word in topic:
       print word[1],
